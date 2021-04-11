c7795cccf989d08875bce3bac2208451bb750133
krishanumandalnavsoft <70763056+krishanumandalnavsoft@users.noreply.github.com>
2021-04-11 21:54:03 +0530

    Add files via upload

--- app/code/krishanunw/Nav/Observer/OrderdataObserver.php
+++ app/code/krishanunw/Nav/Observer/OrderdataObserver.php
@@ -2,18 +2,30 @@
 namespace krishanunw\Nav\Observer; 
 use Magento\Framework\Event\ObserverInterface; 
 use Psr\Log\LoggerInterface; 
+use Krishanu\Nav\Model\KrishanunwTable;
 class OrdedataObserver implements ObserverInterface 
 { 
+protected $_krishanuTable;
 protected $logger; 
-public function __construct(LoggerInterface$logger) 
-{ 
-$this->logger = $logger;
+public function __construct(LoggerInterface$logger,  \Krishanu\Nav\Model\KrishanunwTable  $krishanuTable) 
+{
+  $this->_krishanuTable = $krishanuTable;
+  $this->logger = $logger;
 }
 public function execute(\Magento\Framework\Event\Observer $observer)
 {
  try 
 {
+    $model = $this->_krishanuTable->create();
     $order = $observer->getEvent()->getOrder();
+    $model->addData([
+			"id" => rand(99999),
+			"order_id" => $order,
+			"status" => true,
+			"sort_order" => 1
+			]);
+    $saveData = $model->save();
+
 }
 catch (\Exception $e) 
 {
